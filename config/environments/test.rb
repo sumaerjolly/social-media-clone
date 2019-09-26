0c75577f811ac9360dcc4ba2c0554f2c7abe9954
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-26 23:04:40 +0530

    server working with gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
 # your test database is "scratch space" for the test suite and is wiped
@@ -7,7 +5,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  
   config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
