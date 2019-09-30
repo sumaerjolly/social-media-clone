7c0abe8cd6738078cbb1b4cb9e5d72085871529a
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-30 20:07:22 +0530

    added first and last name to user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   root 'welcome#index'
 end
