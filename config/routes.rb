df8a6a9513016e655fd2996f1100b8cf3ae16b54
ibtechraj <raj_shk@rediffmail.com>
2019-10-01 17:31:13 +0530

    added gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   as :user do
     authenticated :user do
-      root 'posts#index', as: :authenticated_root
+      root 'welcome#index', as: :authenticated_root
     end
     unauthenticated do
       root 'welcome#index', as: :unauthenticated_root
     end
   end
+  resources :users, only: [:show]
 end
