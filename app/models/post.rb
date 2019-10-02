5870653389ccb0d55155878761c2197dd6fedc47
ibtechraj <raj_shk@rediffmail.com>
2019-10-02 19:06:21 +0530

    added profile page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  default_scope -> { order(created_at: :desc) } 
+  
   validates :title, presence: true
   validates :body, presence: true
 end
