e9f826a3a1a4bc057fc5c336d0efa8cdefd1f03f
ibtechraj <raj_shk@rediffmail.com>
2019-10-03 16:21:34 +0530

    testing..

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+    include Devise::Test::ControllerHelpers 
+
+    def setup
+        @user = User.create(first_name: 'Raja', last_name: 'Doe', email: 'testingexample@example.com', date_of_birth: '2001-11-5', gender: 1, password: 'password')
+    end
+
+    test 'should get users index' do 
+        sign_in @user
+        get users_path
+        assert_response :success
+    end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,11 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def sign_in_as(user, password)
+    post  new_user_session_path, params: { session: { email: user.email, password: password } }
+  end
+  
+  
+  
 end
