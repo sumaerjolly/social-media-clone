ef8aa9c4fe5aa6af75f980a19293ee0da3d54eb6
Sumaer Jolly <sumaerjolly@gmail.com>
2019-10-03 15:45:40 +0530

    added user model and post model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :gender, presence: true
+  validates :date_of_birth, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
