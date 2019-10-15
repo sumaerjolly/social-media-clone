13042f2bcf00773d5dc8628c7727f18f8575bc66
Sumaer Jolly <sumaerjolly@gmail.com>
2019-10-15 20:49:43 +0530

    fixed destroy method and added minor changes to views

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -14,9 +14,8 @@ class FriendshipsController < ApplicationController
         @friend1 = Friendship.where(user_id: params[:format], friend_id: current_user.id)
         @friend2 = Friendship.where(user_id: current_user.id, friend_id: params[:format])
         @friend = @friend1 || @friend2
-        byebug
         if @friend.delete_all
-            flash[:success]="You are no longer friends"
+            flash[:danger] = 'You are no longer friends'
         end
         redirect_to users_path
     end
