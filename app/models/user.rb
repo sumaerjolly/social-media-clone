89c43c060067c47c71958070fa06c187eee2ff0b
Sumaer Jolly <sumaerjolly@gmail.com>
2019-10-18 20:13:35 +0530

    rubocop line length errors fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,16 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :friendships
-  has_many :inverse_friendships, -> { where(confirmed: true) }, class_name: 'Friendship', foreign_key: :friend_id, dependent: :destroy
-  has_many :confirmed_friendships, -> { where(confirmed: true) }, class_name: 'Friendship', foreign_key: :user_id, dependent: :destroy
+  has_many :inverse_friendships, -> { where(confirmed: true) }, class_name: 'Friendship', 
+            foreign_key: :friend_id, dependent: :destroy
+  has_many :confirmed_friendships, -> { where(confirmed: true) }, class_name: 'Friendship', 
+            foreign_key: :user_id, dependent: :destroy
   has_many :confirmed_friends, through: :confirmed_friendships, source: :friend
   has_many :confirmed_inverse_friends, through: :inverse_friendships, source: :user
-  has_many :pending_friendships, -> { where(confirmed: nil) }, class_name: 'Friendship', foreign_key: :user_id, dependent: :destroy
-  has_many :pending_inverse_friendships, -> { where(confirmed: nil) }, class_name: 'Friendship', foreign_key: :friend_id, dependent: :destroy
+  has_many :pending_friendships, -> { where(confirmed: nil) }, class_name: 'Friendship', 
+            foreign_key: :user_id, dependent: :destroy
+  has_many :pending_inverse_friendships, -> { where(confirmed: nil) }, class_name: 'Friendship', 
+            foreign_key: :friend_id, dependent: :destroy
   has_many :confirmed_friends_posts, through: :confirmed_friends, source: :posts
   has_many :conirmed_inverse_friends_posts, through: :confirmed_inverse_friends, source: :posts
 
