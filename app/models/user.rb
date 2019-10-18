bfcd7423184c437ee6437477880c591b70f1a9df
Sumaer Jolly <sumaerjolly@gmail.com>
2019-10-18 22:38:51 +0530

    minor error fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,16 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :friendships
-  has_many :inverse_friendships, -> { where(confirmed: true) }, class_name: 'Friendship', 
-            foreign_key: :friend_id, dependent: :destroy
-  has_many :confirmed_friendships, -> { where(confirmed: true) }, class_name: 'Friendship', 
-            foreign_key: :user_id, dependent: :destroy
+  has_many :inverse_friendships, -> { where(confirmed: true) }, class_name: 'Friendship',
+                                                                foreign_key: :friend_id, dependent: :destroy
+  has_many :confirmed_friendships, -> { where(confirmed: true) }, class_name: 'Friendship',
+                                                                  foreign_key: :user_id, dependent: :destroy
   has_many :confirmed_friends, through: :confirmed_friendships, source: :friend
   has_many :confirmed_inverse_friends, through: :inverse_friendships, source: :user
-  has_many :pending_friendships, -> { where(confirmed: nil) }, class_name: 'Friendship', 
-            foreign_key: :user_id, dependent: :destroy
-  has_many :pending_inverse_friendships, -> { where(confirmed: nil) }, class_name: 'Friendship', 
-            foreign_key: :friend_id, dependent: :destroy
+  has_many :pending_friendships, -> { where(confirmed: nil) }, class_name: 'Friendship',
+                                                               foreign_key: :user_id, dependent: :destroy
+  has_many :pending_inverse_friendships, -> { where(confirmed: nil) }, class_name: 'Friendship',
+                                                                       foreign_key: :friend_id, dependent: :destroy
   has_many :confirmed_friends_posts, through: :confirmed_friends, source: :posts
   has_many :conirmed_inverse_friends_posts, through: :confirmed_inverse_friends, source: :posts
 
@@ -70,6 +70,6 @@ class User < ApplicationRecord
   end
 
   def timeline_posts
-    confirmed_friends_posts + conirmed_inverse_friends_posts + posts
+    confirmed_friends_posts + confirmed_inverse_friends_posts + posts
   end
 end
